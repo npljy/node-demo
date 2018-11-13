2a57525c68c84821ca948e6111c463386770a54e
NiePeng <niepengsmile@gmail.com>
2018-11-13 13:36:51 +0800

    git ignore

--- static/start.js
+++ static/start.js
@@ -6,5 +6,6 @@ const path = require('path');
 const { router } = require('../routers/router.js')
 
 const server = http.createServer((req, res) => {
+
     router(req, res)
 }).listen(80, '127.0.0.1');
