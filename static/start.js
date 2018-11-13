5eb81a2c896909bc16660adbb89d9a00105fdbbc
NiePeng <niepengsmile@gmail.com>
2018-11-13 13:35:36 +0800

    npm init

---
+++ static/start.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+const { router } = require('../routers/router.js')
+
+const server = http.createServer((req, res) => {
+    router(req, res)
+}).listen(80, '127.0.0.1');
--- tools/getExtName.js
+++ tools/getExtName.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 exports.getExtName = function(extname) {
-
-    let data = fs.readFileSync('data/ext.json', 'utf-8');
+    // 也可以给 getExtName 增加回调函数，然后使用 readFile 的回调函数实现
+    // 也可以 通过 事件广播 实现
+    let data = fs.readFileSync('../data/ext.json', 'utf-8');
     let jsonObj = JSON.parse(data.toString());
     let contType = jsonObj[extname];
-    console.log("123Content-Type:" + extname + " " + contType)
+    // console.log("123Content-Type:" + extname + " " + contType)
     return contType || "text/html";
 
 }
